259211283414cce028c43a867a1486fd95f56335
Alexis <wollsale@gmail.com>
2020-08-29 19:21:12 -0400

    improve function.php

--- functions.php
+++ functions.php
@@ -7,7 +7,14 @@
 */
 function theme_styles()
 {
-    wp_enqueue_style('main', get_template_directory_uri() . '/assets/style/app.css');
+    //wp_enqueue_style('main', get_template_directory_uri() . '/assets/style/app.css');
+    $cssFilePath = glob(get_template_directory() . '/build/css/main.min.*');
+    $cssFileURI = get_template_directory_uri() . '/build/css/' . basename($cssFilePath[0]);
+    wp_enqueue_style('site_main_css', $cssFileURI);
+    // include the javascript file
+    $jsFilePath = glob(get_template_directory() . '/build/js/app.min.*.js');
+    $jsFileURI = get_template_directory_uri() . '/build/js/' . basename($jsFilePath[0]);
+    wp_enqueue_script('site_main_js', $jsFileURI, null, null, true);
 }
 
 add_action('wp_enqueue_scripts', 'theme_styles');

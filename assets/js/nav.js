71128f2a40b4723db66b48d666b7b97151d2415a
Alexis <wollsale@gmail.com>
2020-11-09 21:54:22 -0500

    Disable javascript trigger

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -18,25 +18,25 @@ export const nav = () => {
     body.classList.remove('no-scroll')
 
     // TRIGGER
-    trigger.forEach(el => {
-        el.addEventListener('click', (e) => {
-            e.preventDefault();
+    // trigger.forEach(el => {
+    //     el.addEventListener('click', (e) => {
+    //         e.preventDefault();
 
-            if (!nav.classList.contains('head-nav--is-active')) {
-                nav.classList.add('head-nav--is-active')
-                container.classList.add('open')
-                console.log('open nav')
-            } else {
-                nav.classList.remove('head-nav--is-active')
-                container.classList.remove('open')
-                console.log('close nav')
-            }
+    //         if (!nav.classList.contains('head-nav--is-active')) {
+    //             nav.classList.add('head-nav--is-active')
+    //             container.classList.add('open')
+    //             console.log('open nav')
+    //         } else {
+    //             nav.classList.remove('head-nav--is-active')
+    //             container.classList.remove('open')
+    //             console.log('close nav')
+    //         }
 
-            if (!body.classList.contains('no-scroll')) {
-                body.classList.add('no-scroll')
-            } else {
-                body.classList.remove('no-scroll')
-            }
-        })
-    });
+    //         if (!body.classList.contains('no-scroll')) {
+    //             body.classList.add('no-scroll')
+    //         } else {
+    //             body.classList.remove('no-scroll')
+    //         }
+    //     })
+    // });
 }

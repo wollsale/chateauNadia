251b856841e7d49980c3c3bc442cb79c28520c28
Alexis <wollsale@gmail.com>
2020-12-15 21:45:56 -0500

    reactivate carousels videos

--- assets/js/app.js
+++ assets/js/app.js
@@ -89,32 +89,32 @@ if (document.querySelector('.modal')) {
 //     buttonHover(document.querySelector('.wp-block-button__link'));
 // }
 
-// window.addEventListener('load', function () {
-//     if (document.querySelector('.carousel')) {
-//         var carousel = tns({
-//             container: '.carousel',
-//             "startIndex": 1,
-//             "loop": true,
-//             items: 1,
-//             "center": true,
-//             gutter: "100px",
-//             rewind: 'true',
-//             touch: true,
-//             controls: true,
-//             nav: false,
-//             controlsContainer: document.querySelector('.carousel__controls'),
-//             prevButton: document.getElementById('#prev'),
-//             nextButton: document.getElementById('#next'),
-//         });
+window.addEventListener('load', function () {
+    if (document.querySelector('.carousel')) {
+        var carousel = tns({
+            container: '.carousel',
+            "startIndex": 1,
+            "loop": true,
+            items: 1,
+            "center": true,
+            gutter: "100px",
+            rewind: 'true',
+            touch: true,
+            controls: true,
+            nav: false,
+            controlsContainer: document.querySelector('.carousel__controls'),
+            prevButton: document.getElementById('#prev'),
+            nextButton: document.getElementById('#next'),
+        });
 
-//         const controls = document.querySelectorAll('.carousel__control');
-//         controls.forEach(control => {
-//             control.addEventListener('click', (e) => {
-//                 e.preventDefault();
-//             })
-//         });
-//     }
-// })
+        const controls = document.querySelectorAll('.carousel__control');
+        controls.forEach(control => {
+            control.addEventListener('click', (e) => {
+                e.preventDefault();
+            })
+        });
+    }
+})
 
 // window.addEventListener('load', function () {
 //     if (document.querySelector('.hero-slider')) {
